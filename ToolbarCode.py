275f52c04fc219027d191e4aeab0fd681859d120
subzero258 <BWsubzero2582@gmail.com>
2022-06-05 13:47:37 -0400

    Toolbar Update
    1. Added seperators between some options in the toolbar
    2. Added the "Find and Replace" option in the edit menu
    3. Added a pop-up menu after right clicking
    4. Added a settings button to the toolbar
    5. Added placeholders for toolbar functionality (currently they just crash when ran)

--- ToolbarCode.py
+++ ToolbarCode.py
@@ -6,6 +6,47 @@ from PyQt6.QtGui import QAction
 
 class Window(QMainWindow):
     """Main Window."""
+    # Action Functionality Placeholder
+    def newFile(self):
+        # Logic for creating a new file goes here...
+        self.centralWidget.setText("<b>File > New</b> clicked")
+    def openFile(self):
+        # Logic for opening an existing file goes here...
+        self.centralWidget.setText("<b>File > Open...</b> clicked")
+    def saveFile(self):
+        # Logic for saving a file goes here...
+        self.centralWidget.setText("<b>File > Save</b> clicked")
+    def copyContent(self):
+        # Logic for copying content goes here...
+        self.centralWidget.setText("<b>Edit > Copy</b> clicked")
+    def pasteContent(self):
+        # Logic for pasting content goes here...
+        self.centralWidget.setText("<b>Edit > Paste</b> clicked")
+    def cutContent(self):
+        # Logic for cutting content goes here...
+        self.centralWidget.setText("<b>Edit > Cut</b> clicked")
+    def helpContent(self):
+        # Logic for launching help goes here...
+        self.centralWidget.setText("<b>Help > Help Content...</b> clicked")
+    def about(self):
+        # Logic for showing an about dialog content goes here...
+        self.centralWidget.setText("<b>Help > About...</b> clicked")
+
+    def _connectActions(self):
+        # Connect File actions
+        self.newAction.triggered.connect(self.newFile)
+        self.openAction.triggered.connect(self.openFile)
+        self.saveAction.triggered.connect(self.saveFile)
+        self.exitAction.triggered.connect(self.close)
+        # Connect Edit actions
+        self.copyAction.triggered.connect(self.copyContent)
+        self.pasteAction.triggered.connect(self.pasteContent)
+        self.cutAction.triggered.connect(self.cutContent)
+        # Connect Help actions
+        self.helpContentAction.triggered.connect(self.helpContent)
+        self.aboutAction.triggered.connect(self.about)
+
+    # Populating Buttons with Actions
     def _createActions(self):
         self.newAction = QAction("&New", self)
         self.openAction = QAction("&Open", self)
@@ -23,12 +64,17 @@ class Window(QMainWindow):
         fileMenu.addAction(self.newAction)
         fileMenu.addAction(self.openAction)
         fileMenu.addAction(self.saveAction)
+        fileMenu.addSeparator()
         fileMenu.addAction(self.exitAction)
 
         editMenu = menuBar.addMenu("&Edit")
         editMenu.addAction(self.copyAction)
         editMenu.addAction(self.pasteAction)
         editMenu.addAction(self.cutAction)
+        editMenu.addSeparator()
+        findMenu = editMenu.addMenu("Find and Replace")
+
+        settingsMenu = menuBar.addMenu("&Settings")
 
         helpMenu = menuBar.addMenu("&Help")
         helpMenu.addAction(self.helpContentAction)
@@ -46,7 +92,19 @@ class Window(QMainWindow):
         mainView.addWidget(fileListW)
         mainView.addLayout(workspace)
         return mainView
+    def _createContextMenu(self):
+        self.centralWidget.setContextMenuPolicy(Qt.ActionsContextMenu)
+        self.centralWidget.addAction(self.newAction)
+        self.centralWidget.addAction(self.openAction)
+        self.centralWidget.addAction(self.saveAction)
+        separator = QAction(self)
+        separator.setSeparator(True)
+        self.centralWidget.addAction(separator)
+        self.centralWidget.addAction(self.copyAction)
+        self.centralWidget.addAction(self.pasteAction)
+        self.centralWidget.addAction(self.cutAction)
 
+    # Creating Window
     def __init__(self, parent=None):
         """Initializer."""
         super().__init__(parent)
@@ -57,6 +115,8 @@ class Window(QMainWindow):
         self.setCentralWidget(self.centralWidget)
         self._createActions()
         self._createMenuBar()
+        self._createContextMenu()
+        self._connectActions()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
