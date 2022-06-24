bfe1d6aaba0da32f5d09481ca9329f64bb553d9a
Jonathan Wilson <tiberius202@gmail.com>
2022-06-24 11:45:34 -0400

    Added action group. broken state

--- ToolbarCode.py
+++ ToolbarCode.py
@@ -4,7 +4,7 @@ import os
 import re
 
 from PyQt6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QPushButton, QAbstractItemView, QListWidget, QListWidgetItem, QTreeWidget, QTreeWidgetItem, QTextEdit, QWidget, QFileDialog
-from PyQt6.QtGui import QAction
+from PyQt6.QtGui import QAction, QActionGroup
 from itertools import *
 import numpy as np
 from collections import Counter
@@ -61,6 +61,63 @@ def count_tokens(tok, non_punct):
     token_counts = list(zip(tokens.tolist(), token_freq.tolist(), token_prop.tolist(), token_range.tolist()))
     token_counts = list(token_counts)
     return(token_counts)
+def count_tags(tok, non_punct):
+    tag_list = []
+    # remove tags for puct, unidentified, and multitoken units
+    remove_starttags = ('Y', 'FU')
+    remove_endtags = ('21', '22', '31', '32', '33', '41', '42', '43', '44')
+    #[x for x in listB for a in listA if a in x]
+    for i in range(0,len(tok)):
+        tags = [x[1] for x in tok[i]]
+        tags = [x for x in tags if not x.startswith(remove_starttags)]
+        tags = [x for x in tags if not x.endswith(remove_endtags)]
+        tag_list.append(tags)
+    tag_range = []
+    for i in range(0,len(tok)):
+        tag_range.append(list(set(tag_list[i])))
+    tag_range = [x for xs in tag_range for x in xs]
+    tag_range = Counter(tag_range)
+    tag_range = sorted(tag_range.items(), key=lambda pair: pair[0], reverse=False)
+    tag_list = [x for xs in tag_list for x in xs]
+    tag_list = Counter(tag_list)
+    tag_list = sorted(tag_list.items(), key=lambda pair: pair[0], reverse=False)
+    tags = np.array([x[0] for x in tag_list])
+    tag_freq = np.array([x[1] for x in tag_list])
+    tag_prop = np.array(tag_freq)/non_punct*100
+    tag_prop = tag_prop.round(decimals=2)
+    tag_range = np.array([x[1] for x in tag_range])/len(tok)*100
+    tag_range = tag_range.round(decimals=2)
+    tag_counts = list(zip(tags.tolist(), tag_freq.tolist(), tag_prop.tolist(), tag_range.tolist()))
+    tag_counts = list(tag_counts)
+    return(tag_counts)
+def count_ds(tok, corpus_total):
+    ds_list = []
+    for i in range(0,len(tok)):
+        ds_cats = [x[2] for x in tok[i]]
+        # filter for benning entity tags
+        ds_cats = [x for x in ds_cats if x.startswith('B-')]
+        ds_list.append(ds_cats)
+    ds_range = []
+    for i in range(0,len(tok)):
+        ds_range.append(list(set(ds_list[i])))
+    ds_range = [x for xs in ds_range for x in xs]
+    ds_range = Counter(ds_range)
+    ds_range = sorted(ds_range.items(), key=lambda pair: pair[0], reverse=False)
+    ds_list = [x for xs in ds_list for x in xs]
+    ds_list = Counter(ds_list)
+    ds_list = sorted(ds_list.items(), key=lambda pair: pair[0], reverse=False)
+    ds_cats = [x[0] for x in ds_list]
+    ds_cats = [x.replace('B-', '') for x in ds_cats]
+    ds_cats = [re.sub(r'([a-z])([A-Z])', '\\1 \\2', x) for x in ds_cats]
+    ds_cats = np.array(ds_cats)
+    ds_freq = np.array([x[1] for x in ds_list])
+    ds_prop = np.array(ds_freq)/corpus_total*100
+    ds_prop = ds_prop.round(decimals=2)
+    ds_range = np.array([x[1] for x in ds_range])/len(tok)*100
+    ds_range = ds_range.round(decimals=2)
+    ds_counts = zip(ds_cats.tolist(), ds_freq.tolist(), ds_prop.tolist(), ds_range.tolist())
+    ds_counts = list(ds_counts)
+    return(ds_counts)
 
 class Window(QMainWindow):
     def runSpacyModel(self):
@@ -214,6 +271,13 @@ class Window(QMainWindow):
         self.documentViewAction.setCheckable(True)
         self.documentViewAction.toggled.connect(self.toggleTextEditor)
         viewMenu.addAction(self.documentViewAction)
+        self.outputFormat = QActionGroup(self)
+        #todo: text
+        self.outputFormat.setExclusionPolicy(QActionGroup.ExclusionPolicy.Exclusive)
+        wordlistAction = QAction("Word List", self)
+        #todo: connect
+        self.outputFormat.addAction(wordlistAction)
+        viewMenu.addAction(self.outputFormat)
         #Settings
         settingsMenu = menuBar.addMenu("Settings")
         #Help TODO link webpages
@@ -261,9 +325,9 @@ class Window(QMainWindow):
         super().__init__(parent)
         self.setWindowTitle("BrownQt Work in Progress")
         self.resize(1280, 720)
-        self.centralWidget = QWidget()
-        self.centralWidget.setLayout(self._createMainView())
-        self.setCentralWidget(self.centralWidget)
+        centralWidget = QWidget()
+        centralWidget.setLayout(self._createMainView())
+        self.setCentralWidget(centralWidget)
         self._createMenuBar()
         #initialize model
         self.nlp = spacy.load(os.path.join(os.path.dirname(__file__) , "model-new"))
@@ -272,7 +336,6 @@ class Window(QMainWindow):
         self.openFilesToBeAdded = []
         self.currFileDict = {}
         self.corp = None
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
